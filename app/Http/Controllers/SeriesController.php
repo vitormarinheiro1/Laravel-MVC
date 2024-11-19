d502abd0618ec557645ce2a1fa5b6709e4113364
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-11-19 14:01:32 -0300

    fix: bug timestamp corrigido e uso do Eloquent ORM

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -9,7 +10,9 @@ class SeriesController extends Controller
 {
     public function index(Request $request)
     {
-        $series = DB::select('SELECT name FROM series;');
+        // $series = DB::select('SELECT name FROM series;');
+
+        $series = Serie::all();
 
         return view('series.index')->with('series', $series);
     }
@@ -21,12 +24,14 @@ class SeriesController extends Controller
 
     public function store(Request $request)
     {
+        
+        // DB::insert('INSERT INTO series (name) VALUES (?)', [$nomeSerie]);
+        
         $nomeSerie = $request->input('name');
+        $serie = new Serie();
+        $serie->name = $nomeSerie;
+        $serie->save();
 
-        if (DB::insert('INSERT INTO series (name) VALUES (?)', [$nomeSerie])) {
-            return "OK";
-        } else {
-            return "Deu erro";
-        }
+        return redirect('/series');
     }
 }
